b9c855916fe96ca19f8fbe276deed8190d162108
Ilyas Shaekhov <fonfabular@gmail.com>
2022-09-27 15:42:54 +0300

    add database dump and some fix in django admin

--- back/core/admin.py
+++ back/core/admin.py
@@ -6,10 +6,25 @@ from django.contrib import admin
 # Register your models here.
 
 
+class CounterpartyAdmin(admin.ModelAdmin):
+	list_display = ('name', 'rating')
+	list_filter = ('rating',)
+	search_fields = ('name',)
+	ordering = ('name',)
+
+
+class DealAdmin(admin.ModelAdmin):
+	list_display = (
+		'type', 'deal_date', 'destination_place', 'amount', 'cost',
+		'start_date', 'end_date', 'inventory', 'counterparty'
+	)
+	list_filter = ('type', 'counterparty', 'destination_place', 'inventory', 'deal_date', 'start_date', 'end_date')
+
+
 admin.site.register(handbook.Rating)
 admin.site.register(handbook.Place)
 admin.site.register(handbook.Inventory)
 admin.site.register(handbook.DealType)
 
-admin.site.register(objects.Counterparty)
-admin.site.register(objects.Deal)
+admin.site.register(objects.Counterparty, CounterpartyAdmin)
+admin.site.register(objects.Deal, DealAdmin)
--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ docker-compose up -d
 cd ..
 docker exec -i test_gz_back python manage.py migrate
 docker exec -i test_gz_back python manage.py initadmin
+docker exec -i test_gz_back python manage.py loaddata db.json
